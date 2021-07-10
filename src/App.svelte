4bb6f5bf9cfc0be993d105953f01b6ab65c83d3a
James Evans <evans.james00@gmail.com>
2021-07-09 22:00:55 -0700

    first draft

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,14 @@
 <script>
+import { Animate } from 'svelte-swipeable';
+  let direction = 'any';
+  let willReturn = false;
+  let stiffness = 0.3;
+  let damping = 0.8;
+  let momentum = 0.2;
+
+  let arr = [1, 2, 3, 4, 5];
+
+  let activeIndex = null;
 </script>
 
 <style>
@@ -12,6 +22,40 @@
     width: 50%;
   }
 
+  .item-wrapper {
+    /* max-width: 900px; */
+    display: flex;
+    justify-content: center;
+    margin: 0 auto; 
+  }
+
+  .item {
+    width: 100px;
+    position: relative;
+    z-index: 0;
+  }
+
+  .keys {
+    margin-right: 50px;
+    display: flex;
+  }
+
+  .teas {
+    margin-left: 50px;
+    display: flex;
+  }
+
+  .grab-wrapper {
+    position: relative;
+  }
+
+  .grabber {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 1000;
+  }
+
 </style>
 
 <div class="wrapper">
@@ -19,3 +63,25 @@
   <img class="big-img grandma" src="images/grandma.png"/>
 </div>
 
+<div class="item-wrapper">
+  <div class="keys">
+  {#each arr as item}
+    <Animate direction={direction} stiffness={stiffness} damping={damping} willReturn={willReturn} momentum={momentum}>
+      <div class="grab-wrapper">
+        <div class="grabber"></div>
+        <img class="item" src="https://i.pinimg.com/originals/eb/1e/bc/eb1ebcf28c7b93762b33b0d06808245b.png"/>
+      </div>
+    </Animate>
+  {/each}
+  </div>
+  <div class="teas">
+  {#each arr as item}
+    <Animate direction={direction} stiffness={stiffness} damping={damping} willReturn={willReturn} momentum={momentum}>
+      <div class="grab-wrapper">
+        <div class="grabber"></div>
+        <img class="item" src="images/tea.png"/>
+      </div>
+    </Animate>
+  {/each}
+  </div>
+</div>
