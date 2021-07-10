94bbaedd90c4ad5e781ec775c59bb9e13c58d40e
James Evans <evans.james00@gmail.com>
2021-07-09 21:20:16 -0700

    pics

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,21 @@
 <script>
 </script>
 
-<h1>
-Hi Lauren!!!
-</h1>
+<style>
+  .wrapper {
+    max-width: 900px;
+    display: flex;
+    justify-content: space-between;
+    margin: 0 auto;
+  }
+  .big-img {
+    width: 50%;
+  }
+
+</style>
+
+<div class="wrapper">
+  <img class="big-img door" src="https://thumbs.dreamstime.com/b/front-door-house-entrace-icon-cartoon-vector-illustration-graphic-design-front-door-house-entrace-icon-151525317.jpg"/>
+  <img class="big-img grandma" src="images/grandma.png"/>
+</div>
+
